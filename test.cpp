261a2943c599a49732dc37e889ec394266c7faed
LuongDucAnh20020626 <20020626@vnu.edu.vn>
2022-09-14 19:41:27 +0700

    Upload test.cpp

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int binarySearch (int arr[],int left, int right,int index){
+    if (left < right) {
+        int mid = (left + right)/2;
+        if (arr[mid] == index) return mid;
+        if (arr[mid] < index) return binarySearch(arr, mid + 1, right, index);
+        if (arr[mid] > index) return binarySearch(arr, left, mid - 1, index);
+    }
+    return -1;
+}
+int main () {
+    int n , s;
+    cin >> n >> s;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    if (binarySearch(arr,0,n,s)>-1) {
+        cout<<"YES";
+    }
+    else cout<<"NO";
+    return 0;
+}
